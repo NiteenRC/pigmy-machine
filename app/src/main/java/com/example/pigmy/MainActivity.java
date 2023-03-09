b9f9e88a624f9a6b8ed748289326b36689418f9b
Satish Chougule <satishrajc@gmail.com>
2023-03-09 22:22:20 +0530

    validation

--- app/src/main/java/com/example/pigmy/MainActivity.java
+++ app/src/main/java/com/example/pigmy/MainActivity.java
@@ -13,12 +13,12 @@ import android.os.Environment;
 import android.provider.Settings;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
@@ -49,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
     private int receiptNo;
     private double prevAmount;
     private double depositAmount;
+    private AutoCompleteTextView autoCompleteTextView;
 
     @SuppressLint("SetTextI18n")
     @Override
@@ -77,7 +78,7 @@ public class MainActivity extends AppCompatActivity {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
                 list.forEach(x -> map.put(x.split(",")[5].trim() + " " + x.split(",")[1].trim() + " " + x.split(",")[3].trim(), x));
                 List<String> accNos = new ArrayList<>(map.keySet());
-                final AutoCompleteTextView autoCompleteTextView = findViewById(R.id.autoCompleteTextView1);
+                autoCompleteTextView = findViewById(R.id.autoCompleteTextView1);
                 ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, accNos);
                 autoCompleteTextView.setAdapter(arrayAdapter);
                 autoCompleteTextView.setThreshold(0);
@@ -93,6 +94,88 @@ public class MainActivity extends AppCompatActivity {
                     textView.setText("");
                     resultTextView.setText("");
                     autoCompleteTextView.setText("");
+                    prevAmount = 0;
+                });
+
+                Button addButton = findViewById(R.id.add_button);
+                addButton.setOnClickListener(view -> {
+                    final String customerNameAuto = autoCompleteTextView.getText().toString();
+                    final String name = resultTextView.getText().toString();
+
+                    if (customerNameAuto.length() == 0) {
+                        autoCompleteTextView.requestFocus();
+                        autoCompleteTextView.setError("FIELD CANNOT BE EMPTY");
+                    } else if (name.length() == 0) {
+                        resultTextView.requestFocus();
+                        resultTextView.setError("FIELD CANNOT BE EMPTY");
+                    } else {
+                        Path path;
+                        try {
+                            path = createFile("receive.txt");
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                        if (list.isEmpty()) {
+                            list.add("000,12345,0000000000002900.00,00000000000000000009,0000000006,16.02.23,12345");
+                        }
+
+                        //000,+,PIGMY,0001               ,0000000.00,GANGADAR BADIGER    ,0000100.00,27.12.22,0000000.00@
+                        //000,PIGMY,0249               ,SRINIVASH MADAG     ,0000100.00,16.02.23,00005
+                        String creditOrDebit = selectedRow.split(",")[1];
+                        String accType = selectedRow.split(",")[2];
+                        String accNo = selectedRow.split(",")[3];
+                        String customerName = selectedRow.split(",")[5];
+
+                        String validateCustomer = customerName.trim() + " " + creditOrDebit + " " + accNo.trim();
+                        if (!customerNameAuto.equals(validateCustomer)) {
+                            autoCompleteTextView.requestFocus();
+                            autoCompleteTextView.setError("Select valid customer");
+                            return;
+                        }
+
+                        String depositAmountSuffix = String.valueOf(depositAmount);
+
+                        String currentAmount = "";
+                        if (depositAmountSuffix.length() == 9) {
+                            currentAmount = String.format("%0" + (10 - depositAmountSuffix.length()) + "d%s", 0, depositAmountSuffix);
+                        } else {
+                            currentAmount = String.format("%0" + (9 - depositAmountSuffix.length()) + "d%s", 0, depositAmountSuffix);
+                            currentAmount = currentAmount.split("\\.")[1].length() == 1 ? currentAmount + "0" : currentAmount;
+                        }
+
+                        if (currentAmount.length() == 9) {
+                            currentAmount = "0" + currentAmount;
+                        }
+
+                        @SuppressLint("SimpleDateFormat") String date = new SimpleDateFormat("dd.MM.yy").format(new Date());
+                        @SuppressLint("SimpleDateFormat") String dateAndTime = new SimpleDateFormat("dd.MM.yy hh:mm:ss").format(new Date());
+
+                        receiptNo++;
+                        String receipt = String.format("%0" + (5 - String.valueOf(receiptNo).length()) + "d%s", 0, receiptNo);
+
+                        list.add("000," + accType + "," + accNo + "," + customerName + "," + currentAmount + "," + date + "," + receipt);
+
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                            try {
+                                Files.write(path, list, StandardCharsets.UTF_8, StandardOpenOption.APPEND);
+                            } catch (IOException e) {
+                                throw new RuntimeException(e);
+                            }
+                        }
+
+                        String s = list.get(list.size() - 1);
+                        Intent intent = new Intent(MainActivity.this, CustomerReceipt.class);
+                        intent.putExtra("date_time", dateAndTime);
+                        intent.putExtra("agent_code", "12345");
+                        intent.putExtra("acc_type", accType.trim());
+                        intent.putExtra("customer_name", customerName.trim());
+                        intent.putExtra("acc_no", accNo.trim());
+                        intent.putExtra("prev_balance", String.valueOf(prevAmount));
+                        intent.putExtra("deposit_amount", String.valueOf(depositAmount));
+                        intent.putExtra("total_amount", String.valueOf(totalAmount));
+                        startActivity(intent);
+                        Toast.makeText(MainActivity.this, "Saved Successful", Toast.LENGTH_LONG).show();
+                    }
                 });
             }
         }
@@ -124,6 +207,11 @@ public class MainActivity extends AppCompatActivity {
                 if (isDigitsOnly(s) && s.length() > 0) {
                     depositAmount = Double.parseDouble(s.toString());
 
+                    if (selectedRow == null) {
+                        autoCompleteTextView.requestFocus();
+                        autoCompleteTextView.setError("Select valid customer");
+                        return;
+                    }
                     String creditOrDebit = selectedRow.split(",")[1];
                     totalAmount = creditOrDebit.equals("+") ? (prevAmount + depositAmount) : (prevAmount - depositAmount);
 
@@ -157,6 +245,7 @@ public class MainActivity extends AppCompatActivity {
                     accountTypeView.setText("Acc Type: ");
                     textView.setText("");
                     resultTextView.setText("");
+                    prevAmount = 0;
                 }
             }
 
@@ -198,57 +287,6 @@ public class MainActivity extends AppCompatActivity {
         Uri uri = Uri.fromParts("package", getPackageName(), null);
         intent.setData(uri);
         startActivityForResult(intent, MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
-    }
-
-    public void onAddButtonClick(View view) throws IOException {
-        Path path = createFile("receive.txt");
-        if (list.isEmpty()) {
-            list.add("000,12345,0000000000002900.00,00000000000000000009,0000000006,16.02.23,12345");
-        }
-
-        //000,+,PIGMY,0001               ,0000000.00,GANGADAR BADIGER    ,0000100.00,27.12.22,0000000.00@
-        //000,PIGMY,0249               ,SRINIVASH MADAG     ,0000100.00,16.02.23,00005
-        String accType = selectedRow.split(",")[2];
-        String accNo = selectedRow.split(",")[3];
-        String customerName = selectedRow.split(",")[5];
-
-        String depositAmountSuffix = String.valueOf(depositAmount);
-
-        String currentAmount = "";
-        if (depositAmountSuffix.length() == 9) {
-            currentAmount = String.format("%0" + (10 - depositAmountSuffix.length()) + "d%s", 0, depositAmountSuffix);
-        } else {
-            currentAmount = String.format("%0" + (9 - depositAmountSuffix.length()) + "d%s", 0, depositAmountSuffix);
-            currentAmount = currentAmount.split("\\.")[1].length() == 1 ? currentAmount + "0" : currentAmount;
-        }
-
-        if (currentAmount.length() == 9) {
-            currentAmount = "0" + currentAmount;
-        }
-
-        @SuppressLint("SimpleDateFormat") String date = new SimpleDateFormat("dd.MM.yy").format(new Date());
-        @SuppressLint("SimpleDateFormat") String dateAndTime = new SimpleDateFormat("dd.MM.yy hh:mm:ss").format(new Date());
-
-        receiptNo++;
-        String receipt = String.format("%0" + (5 - String.valueOf(receiptNo).length()) + "d%s", 0, receiptNo);
-
-        list.add("000," + accType + "," + accNo + "," + customerName + "," + currentAmount + "," + date + "," + receipt);
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            Files.write(path, list, StandardCharsets.UTF_8, StandardOpenOption.APPEND);
-        }
-
-        String s = list.get(list.size() - 1);
-        Intent intent = new Intent(MainActivity.this, CustomerReceipt.class);
-        intent.putExtra("date_time", dateAndTime);
-        intent.putExtra("agent_code", "12345");
-        intent.putExtra("acc_type", accType.trim());
-        intent.putExtra("customer_name", customerName.trim());
-        intent.putExtra("acc_no", accNo.trim());
-        intent.putExtra("prev_balance", String.valueOf(prevAmount));
-        intent.putExtra("deposit_amount", String.valueOf(depositAmount));
-        intent.putExtra("total_amount", String.valueOf(totalAmount));
-        startActivity(intent);
     }
 
     private Path createFile(String fileName) throws IOException {
