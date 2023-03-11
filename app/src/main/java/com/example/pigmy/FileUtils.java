2f96b017019d21ce87a67d2c8072728b7c5ad807
Rajesh <rajesh@version.in>
2023-03-11 22:26:49 +0530

    fixed storage permission issue and code refactoring

---
+++ app/src/main/java/com/example/pigmy/FileUtils.java
@@ -0,0 +1,65 @@
+package com.example.pigmy;
+
+import android.os.Environment;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class FileUtils {
+
+    protected static String createFile(String fileName){
+        File downloadPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+        File file = new File(downloadPath,fileName);
+        return file.getAbsolutePath();
+    }
+
+    protected boolean writeToFile(List<String> stringList){
+        boolean writeResult;
+        try {
+            File downloadPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+            File file = new File(downloadPath,"receive.txt");
+            FileOutputStream fOut = new FileOutputStream(file);
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fOut));
+            for (int i = 0; i < stringList.size(); i++) {
+                bw.write(stringList.get(i));
+                bw.newLine();
+            }
+            bw.close();
+            fOut.close();
+            writeResult=true;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return writeResult;
+    }
+
+    protected List<String> readDataExternal() {
+        List<String> dataList = new ArrayList<>();
+        try {
+            File downloadPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+            FileInputStream fis=new FileInputStream(downloadPath +"/send.txt");
+            Scanner sc=new Scanner(fis);
+            while(sc.hasNextLine())
+            {
+                dataList.add(sc.nextLine());
+            }
+            sc.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return dataList;
+    }
+
+}
--- app/src/main/java/com/example/pigmy/MainActivity.java
+++ app/src/main/java/com/example/pigmy/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.pigmy;
 
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 import static android.text.TextUtils.isDigitsOnly;
 
 import android.Manifest;
@@ -13,6 +15,7 @@ import android.os.Environment;
 import android.provider.Settings;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
@@ -20,18 +23,28 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.app.ActivityOptionsCompat;
 import androidx.core.content.ContextCompat;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.security.Permission;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -39,145 +52,42 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 123;
     private final Map<String, String> map = new HashMap<>();
     private String selectedRow;
-    private double totalAmount;
+    private double totalAmount,prevAmount,depositAmount;
     private int receiptNo;
-    private double prevAmount;
-    private double depositAmount;
     private AutoCompleteTextView autoCompleteTextView;
+
+    private TextView textView,accountTypeView,editText;
+    private EditText resultTextView;
+
+    private List<String> dataList = new ArrayList<>();
+
+    private ActivityResultLauncher<Intent> activityResultLauncher =
+            registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                        if (Environment.isExternalStorageManager())
+                        {
+                            initGUI();
+                        }
+                    }
+                }
+            });
 
     @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView textView = findViewById(R.id.total_amount);
-        EditText resultTextView = findViewById(R.id.number2_edit_text);
 
-        TextView editText = findViewById(R.id.number1_edit_text);
-        editText.setText("PREV AMT: ");
+        askPermission();
 
-        final TextView accountTypeView = findViewById(R.id.accountType);
-        accountTypeView.setText("ACC TYPE: ");
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            askPermission();
-        }
-        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
-
-        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
-
-            //Need to check permission
-            List<String> list = readDataExternal();
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                list.forEach(x -> map.put(x.split(",")[5].trim() + " " + x.split(",")[1].trim() + " " + x.split(",")[3].trim(), x));
-                List<String> accNos = new ArrayList<>(map.keySet());
-                autoCompleteTextView = findViewById(R.id.autoCompleteTextView1);
-                ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, accNos);
-                autoCompleteTextView.setAdapter(arrayAdapter);
-                autoCompleteTextView.setThreshold(0);
-
-                resetAccTypeAndPrevAmt(editText, accountTypeView, autoCompleteTextView, textView, resultTextView);
-                onTextChangedForAccountSelection(textView, resultTextView, editText, accountTypeView, autoCompleteTextView);
-                onTextChangedForDepositAmt(textView, resultTextView);
-
-                Button btn = findViewById(R.id.clear_button);
-                btn.setOnClickListener(v -> {
-                    editText.setText("PREV AMT: ");
-                    accountTypeView.setText("ACC TYPE: ");
-                    textView.setText("");
-                    resultTextView.setText("");
-                    autoCompleteTextView.setText("");
-                    prevAmount = 0;
-                });
-
-                Button addButton = findViewById(R.id.add_button);
-                addButton.setOnClickListener(view -> {
-                    final String customerNameAuto = autoCompleteTextView.getText().toString();
-                    final String name = resultTextView.getText().toString();
-
-                    if (customerNameAuto.length() == 0) {
-                        autoCompleteTextView.requestFocus();
-                        autoCompleteTextView.setError("FIELD CANNOT BE EMPTY");
-                    } else if (name.length() == 0) {
-                        resultTextView.requestFocus();
-                        resultTextView.setError("FIELD CANNOT BE EMPTY");
-                    } else {
-                        Path path;
-                        try {
-                            path = createFile("receive.txt");
-                        } catch (IOException e) {
-                            throw new RuntimeException(e);
-                        }
-                        if (list.isEmpty()) {
-                            list.add("000,12345,0000000000002900.00,00000000000000000009,0000000006,16.02.23,12345");
-                        }
-
-                        //000,+,PIGMY,0001               ,0000000.00,GANGADAR BADIGER    ,0000100.00,27.12.22,0000000.00@
-                        //000,PIGMY,0249               ,SRINIVASH MADAG     ,0000100.00,16.02.23,00005
-                        String creditOrDebit = selectedRow.split(",")[1];
-                        String accType = selectedRow.split(",")[2];
-                        String accNo = selectedRow.split(",")[3];
-                        String customerName = selectedRow.split(",")[5];
-
-                        String validateCustomer = customerName.trim() + " " + creditOrDebit + " " + accNo.trim();
-                        if (!customerNameAuto.equals(validateCustomer)) {
-                            autoCompleteTextView.requestFocus();
-                            autoCompleteTextView.setError("SELECT VALID CUSTOMER");
-                            return;
-                        }
-
-                        String depositAmountSuffix = String.valueOf(depositAmount);
-
-                        String currentAmount = "";
-                        if (depositAmountSuffix.length() == 9) {
-                            currentAmount = String.format("%0" + (10 - depositAmountSuffix.length()) + "d%s", 0, depositAmountSuffix);
-                        } else {
-                            currentAmount = String.format("%0" + (9 - depositAmountSuffix.length()) + "d%s", 0, depositAmountSuffix);
-                            currentAmount = currentAmount.split("\\.")[1].length() == 1 ? currentAmount + "0" : currentAmount;
-                        }
-
-                        if (currentAmount.length() == 9) {
-                            currentAmount = "0" + currentAmount;
-                        }
-
-                        @SuppressLint("SimpleDateFormat") String date = new SimpleDateFormat("dd.MM.yy").format(new Date());
-                        @SuppressLint("SimpleDateFormat") String dateAndTime = new SimpleDateFormat("dd.MM.yy hh:mm:ss").format(new Date());
-
-                        receiptNo++;
-                        String receipt = String.format("%0" + (5 - String.valueOf(receiptNo).length()) + "d%s", 0, receiptNo);
-
-                        list.add("000," + accType + "," + accNo + "," + customerName + "," + currentAmount + "," + date + "," + receipt);
-
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                            try {
-                                Files.write(path, list, StandardCharsets.UTF_8, StandardOpenOption.APPEND);
-                            } catch (IOException e) {
-                                throw new RuntimeException(e);
-                            }
-                        }
-
-                        String s = list.get(list.size() - 1);
-                        Intent intent = new Intent(MainActivity.this, CustomerReceipt.class);
-                        intent.putExtra("date_time", dateAndTime);
-                        intent.putExtra("agent_code", "12345");
-                        intent.putExtra("acc_type", accType.trim());
-                        intent.putExtra("customer_name", customerName.trim());
-                        intent.putExtra("acc_no", accNo.trim());
-                        intent.putExtra("prev_balance", String.valueOf(prevAmount));
-                        intent.putExtra("deposit_amount", String.valueOf(depositAmount));
-                        intent.putExtra("total_amount", String.valueOf(totalAmount));
-                        startActivity(intent);
-                        Toast.makeText(MainActivity.this, "Saved Successfully", Toast.LENGTH_LONG).show();
-                    }
-                });
-            }
-        }
     }
 
     @SuppressLint("SetTextI18n")
@@ -254,55 +164,158 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private List<String> readDataExternal() {
-        try {
-            File downloadPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
-            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-                Path path = Paths.get(downloadPath + "/send.txt");
-                return Files.readAllLines(path);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return Collections.emptyList();
-    }
-
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 // Permission has been granted
-                readDataExternal();
+                initGUI();
             } else {
                 // Permission denied
             }
         }
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.R)
     private void askPermission() {
-        Intent intent = new Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
-        Uri uri = Uri.fromParts("package", getPackageName(), null);
-        intent.setData(uri);
-        startActivityForResult(intent, MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+        //checking external storage permission is given or not...
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            if (!Environment.isExternalStorageManager()) {
+                Intent intent = new Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
+                Uri uri = Uri.fromParts("package", getPackageName(), null);
+                intent.setData(uri);
+                activityResultLauncher.launch(intent);
+            } else {
+                initGUI();
+            }
+        } else if (ContextCompat.checkSelfPermission(MainActivity.this,
+                WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{WRITE_EXTERNAL_STORAGE,
+                            READ_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+        } else if (ContextCompat.checkSelfPermission(MainActivity.this,
+                WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            initGUI();
+        }
     }
 
-    private Path createFile(String fileName) throws IOException {
-        Path path = null;
-        File downloadPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-            path = Paths.get(downloadPath + "/" + fileName);
-            Files.deleteIfExists(path);
+    private void initGUI() {
+        textView = findViewById(R.id.total_amount);
+        resultTextView = findViewById(R.id.number2_edit_text);
 
-            if (!Files.exists(path)) {
-                try {
-                    Files.createFile(path);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
+        editText = findViewById(R.id.number1_edit_text);
+        editText.setText("PREV AMT: ");
+
+        accountTypeView = findViewById(R.id.accountType);
+        accountTypeView.setText("ACC TYPE: ");
+
+        dataList = new FileUtils().readDataExternal();
+        for (String item : dataList) {
+            map.put(item.split(",")[5].trim() + " " + item.split(",")[1].trim() + " " + item.split(",")[3].trim(), item);
         }
-        return path;
+
+        List<String> accNos = new ArrayList<>(map.keySet());
+        autoCompleteTextView = findViewById(R.id.autoCompleteTextView1);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, accNos);
+        autoCompleteTextView.setAdapter(arrayAdapter);
+        autoCompleteTextView.setThreshold(0);
+
+        resetAccTypeAndPrevAmt(editText, accountTypeView, autoCompleteTextView, textView, resultTextView);
+        onTextChangedForAccountSelection(textView, resultTextView, editText, accountTypeView, autoCompleteTextView);
+        onTextChangedForDepositAmt(textView, resultTextView);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.clear_button:
+                clearForm();
+                break;
+            case R.id.add_button:
+                saveRecord();
+                break;
+        }
+    }
+
+    private void saveRecord() {
+        final String customerNameAuto = autoCompleteTextView.getText().toString();
+        final String name = resultTextView.getText().toString();
+
+        if (customerNameAuto.length() == 0) {
+            autoCompleteTextView.requestFocus();
+            autoCompleteTextView.setError("FIELD CANNOT BE EMPTY");
+        } else if (name.length() == 0) {
+            resultTextView.requestFocus();
+            resultTextView.setError("FIELD CANNOT BE EMPTY");
+        } else {
+            if (dataList.isEmpty()) {
+                dataList.add("000,12345,0000000000002900.00,00000000000000000009,0000000006,16.02.23,12345");
+            }
+
+            //000,+,PIGMY,0001               ,0000000.00,GANGADAR BADIGER    ,0000100.00,27.12.22,0000000.00@
+            //000,PIGMY,0249               ,SRINIVASH MADAG     ,0000100.00,16.02.23,00005
+            String creditOrDebit = selectedRow.split(",")[1];
+            String accType = selectedRow.split(",")[2];
+            String accNo = selectedRow.split(",")[3];
+            String customerName = selectedRow.split(",")[5];
+
+            String validateCustomer = customerName.trim() + " " + creditOrDebit + " " + accNo.trim();
+            if (!customerNameAuto.equals(validateCustomer)) {
+                autoCompleteTextView.requestFocus();
+                autoCompleteTextView.setError("SELECT VALID CUSTOMER");
+                return;
+            }
+
+            String depositAmountSuffix = String.valueOf(depositAmount);
+
+            String currentAmount = "";
+            if (depositAmountSuffix.length() == 9) {
+                currentAmount = String.format("%0" + (10 - depositAmountSuffix.length()) + "d%s", 0, depositAmountSuffix);
+            } else {
+                currentAmount = String.format("%0" + (9 - depositAmountSuffix.length()) + "d%s", 0, depositAmountSuffix);
+                currentAmount = currentAmount.split("\\.")[1].length() == 1 ? currentAmount + "0" : currentAmount;
+            }
+
+            if (currentAmount.length() == 9) {
+                currentAmount = "0" + currentAmount;
+            }
+
+            @SuppressLint("SimpleDateFormat") String date = new SimpleDateFormat("dd.MM.yy").format(new Date());
+            @SuppressLint("SimpleDateFormat") String dateAndTime = new SimpleDateFormat("dd.MM.yy hh:mm:ss").format(new Date());
+
+            receiptNo++;
+            String receipt = String.format("%0" + (5 - String.valueOf(receiptNo).length()) + "d%s", 0, receiptNo);
+
+            dataList.add("000," + accType + "," + accNo + "," + customerName + "," + currentAmount + "," + date + "," + receipt);
+
+            //writing to file...
+            new FileUtils().writeToFile(dataList);
+
+            clearForm();
+
+            String s = dataList.get(dataList.size() - 1);
+            Intent intent = new Intent(MainActivity.this, CustomerReceipt.class);
+            intent.putExtra("date_time", dateAndTime);
+            intent.putExtra("agent_code", "12345");
+            intent.putExtra("acc_type", accType.trim());
+            intent.putExtra("customer_name", customerName.trim());
+            intent.putExtra("acc_no", accNo.trim());
+            intent.putExtra("prev_balance", String.valueOf(prevAmount));
+            intent.putExtra("deposit_amount", String.valueOf(depositAmount));
+            intent.putExtra("total_amount", String.valueOf(totalAmount));
+            startActivity(intent);
+
+            Toast.makeText(MainActivity.this, "Saved Successfully", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void clearForm() {
+        editText.setText("PREV AMT: ");
+        accountTypeView.setText("ACC TYPE: ");
+        textView.setText("");
+        resultTextView.setText("");
+        autoCompleteTextView.setText("");
+        prevAmount = 0;
     }
 }
